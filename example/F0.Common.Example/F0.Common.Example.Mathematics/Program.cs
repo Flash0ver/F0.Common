cd83f2187eef9333edd898b2a29f8ad0ee15bf4e
Flash0ver <38893694+Flash0ver@users.noreply.github.com>
2018-10-24 22:58:44 +0200

    Added samples
    Samples .NET Core console application

---
+++ example/F0.Common.Example/F0.Common.Example.Mathematics/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using F0.Mathematics;
+
+namespace F0.Common.Example.Mathematics
+{
+	internal static class Program
+	{
+		private static void Main(string[] args)
+		{
+			Console.WriteLine("F0.Common");
+			Console.WriteLine();
+
+			int integer = 240;
+			Console.WriteLine($"{nameof(integer)} {integer} is {(Parity.IsEven(integer) ? "" : "not ")}even.");
+			Console.WriteLine($"{nameof(integer)} {integer} is {(Parity.IsOdd(integer) ? "" : "not ")}odd.");
+		}
+	}
+}
