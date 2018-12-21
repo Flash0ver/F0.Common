f7b2c34791c764c2468700e4f18a09e31879247c
Flash0ver <38893694+Flash0ver@users.noreply.github.com>
2018-12-21 21:21:26 +0100

    Changed Mathematics example
    read integer from Console

--- example/F0.Common.Example/F0.Common.Example.Mathematics/Program.cs
+++ example/F0.Common.Example/F0.Common.Example.Mathematics/Program.cs
@@ -10,9 +10,23 @@ namespace F0.Common.Example.Mathematics
 			Console.WriteLine("F0.Common");
 			Console.WriteLine();
 
-			int integer = 240;
+			int integer = GetInteger();
 			Console.WriteLine($"{nameof(integer)} {integer} is {(Parity.IsEven(integer) ? "" : "not ")}even.");
 			Console.WriteLine($"{nameof(integer)} {integer} is {(Parity.IsOdd(integer) ? "" : "not ")}odd.");
+		}
+
+		private static int GetInteger()
+		{
+			Console.Write("Enter an integer: ");
+			string input = Console.ReadLine();
+
+			if (!Int32.TryParse(input, out int integer))
+			{
+				integer = 240;
+				Console.WriteLine($"'{input}' is not an {typeof(int)}. Using '{integer}'.");
+			}
+
+			return integer;
 		}
 	}
 }
