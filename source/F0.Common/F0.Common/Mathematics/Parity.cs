61c8fe63f9157da1ea890d14e9638078df3965fb
Flash0ver <38893694+Flash0ver@users.noreply.github.com>
2019-01-02 08:34:04 +0100

    Improve readability of Parity code
    has no notable performance impact

--- source/F0.Common/F0.Common/Mathematics/Parity.cs
+++ source/F0.Common/F0.Common/Mathematics/Parity.cs
@@ -2,16 +2,14 @@
 {
 	public static class Parity
 	{
-		private const int two = 2;
-
 		public static bool IsEven(int integer)
 		{
-			return integer % two == 0;
+			return integer % 2 == 0;
 		}
 
 		public static bool IsOdd(int integer)
 		{
-			return integer % two != 0;
+			return integer % 2 != 0;
 		}
 	}
 }
